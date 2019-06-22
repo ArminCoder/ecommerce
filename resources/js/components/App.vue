0388bc5638cfaf47a11783ed78c284dd8c7083b5
ArminCoder <armincoding@gmail.com>
2019-06-22 13:21:58 +0200

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container-fluid">
+        <navbar></navbar>
+       <h1>App Vue Component</h1>
+    </div>
+</template>
+
+<script>
+    import navbar from './navigation/Navbar.vue'
+
+    export default {
+        components: {
+            navbar
+        }
+    };
+</script>
