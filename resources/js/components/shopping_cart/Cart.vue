484764cc41ffbb8bd6a9f66bc67013521c99a89d
ArminCoder <armincoding@gmail.com>
2019-07-10 15:35:12 +0200

    buildin shopping cart front end

---
+++ resources/js/components/shopping_cart/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+        <navbar></navbar>
+		<div id="cartContent" class="container-fluid">
+			Shopping Cart
+		</div>
+        <productsFooter></productsFooter> 
+	</div>
+</template>
+
+<script>
+import navbar from '../navigation/Navbar.vue';
+import productsFooter from '../footer/Footer.vue';
+
+
+export default {
+  components: {
+  	navbar, productsFooter
+  },
+  data () {
+    return {
+
+    }
+  }
+};
+</script>
+
+<style scoped>
+	#cartContent {
+		padding-top: 10vh;
+	 	min-height: 80vh;
+	}
+</style>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,8 +1,11 @@
 import app from './components/App.vue';
 import product from './components/main/Product.vue';
+import cart from './components/shopping_cart/Cart.vue';
+
 
 export default [
 	{path: '/' , component: app},
 	{path: '/products/:id' , name: 'product',  component: product},
+	{path: '/cart' , name: 'cart',  component: cart},
 ]
 
